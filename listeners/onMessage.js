4bf7881c71b69ab155660f5ed45ba0c8d86441b0
reecehunter <heecerunter@gmail.com>
2023-05-18 09:16:03 -0600

    Added the ?closeticket command

--- listeners/onMessage.js
+++ listeners/onMessage.js
@@ -20,6 +20,9 @@ module.exports = {
             // Automatic "bosshub" reaction
             if(message.content.toLowerCase().includes("bosshub")) {
                 message.react(process.env.BOSSHUBEMOJI);
+            } else if(message.content.toLowerCase() == "?closeticket") {
+                message.channel.send("Hello just checking in to make sure your concern been addressed.\n\nIf it has, please close this ticket by clicking the button at the top of this conversation.\n\nIf it hasn't, please respond to this message within 12 hours of your last message or the ticket will be closed and the problem will be considered solved.");
+                message.delete();
             }
         }
 
