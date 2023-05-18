1e225f2198782a3ba6d651a52418a81e2e183d7d
reecehunter <heecerunter@gmail.com>
2023-05-18 09:50:22 -0600

    Added bot status

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const { autoReact } = require("./listeners/onMessage");
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
+  client.user.setActivity("BossHub.net"); 
 });
 
 client.commands = new Collection();
